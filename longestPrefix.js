3c6b35c79935131422f497c88106523d2fbe5667
Jovan Jevtic <jovanjevtic132@gmail.com>
2020-09-13 13:29:13 +0200

    Longest Prefix

---
+++ longestPrefix.js
@@ -0,0 +1,24 @@
+const longestCommonPrefix = (strs) => {
+    if (strs === 0) return ""                                                                                                                                                                       
+    
+    const str = strs[0]
+    
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            
+            const next = strs[j]
+            
+            if (next[i] !== str[i]) {
+                return next.slice(0, i)
+            }
+        }
+    }
+
+    return str
+};
+
+
+//* Testing
+const strs = ['food', 'foo', 'foobalo']
+let result = longestCommonPrefix(strs)
+console.log(result)
